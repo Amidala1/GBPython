4cd9ac52f7ece8693c4bd2f0ec292407082ffdd6
Amidala1 <62018230+Amidala1@users.noreply.github.com>
2021-02-28 12:05:37 +0300

    Update Lesson_5_3.py

--- Lesson5/Lesson_5_3.py
+++ Lesson5/Lesson_5_3.py
@@ -11,7 +11,7 @@
 from os import path
 
 current_dir_name = path.dirname(__file__)
-file_name = path.join(current_dir_name, r"related_files/Lesson_5_3.txt")
+file_name = path.join(current_dir_name, r"example_files/Lesson_5_3.txt")
 
 threshold = 20000
 lower_list = []
